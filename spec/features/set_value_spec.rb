ce4ed441320065f2855b0d790c87f3d582fe25dc
BenRoss92 <benjaminjackross@gmail.com>
2016-10-17 13:14:51 +0100

    setting up set_value spec

--- spec/features/set_value_spec.rb
+++ spec/features/set_value_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 feature 'setting the key and value' do
 
   scenario 'entering a value into form' do
-    
+    visit('/')
+    expect(page).to have_content('Please enter your key and value below')
   end
 
 end
